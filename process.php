f2eb08d89ab6a4de4e6779af469f1ca9d00d83b0
symac <web@geobib.fr>
2020-03-12 14:24:43 +0100

    Premiers essais de génération d'image à partir d'un ISBN avec bandeau de provenance

---
+++ process.php
@@ -0,0 +1,41 @@
+<?php
+    require_once('vendor/autoload.php');
+    use Scriptotek\Sru\Client as SruClient;
+
+    try {
+        $sru = new SruClient('http://catalogue.bnf.fr/api/SRU', [
+            'schema' => 'unimarcXchange',
+            'version' => '1.2'
+        ]);
+    } catch (ErrorException $e) {
+        print "ERREUR ! ";
+        exit;
+    }
+
+    # https://github.com/hackathonBnF/hackathon2016/wiki/API-Couverture-Service
+
+
+$records = $sru->all('bib.fuzzyIsbn adj "978-2-246-81933-2"');
+    # $records = $sru->all('alma.title="Hello world"');
+    foreach ($records as $record) {
+        $ark = $record->data->xpath('./child::*')[0]->attr("id");
+        $url = sprintf("https://catalogue.bnf.fr/couverture?&appName=NE&idArk=%s&couverture=1", ($ark));
+        $coverBnf = imagecreatefromjpeg($url);
+        $x = imagesx($coverBnf );
+        $y = imagesy($coverBnf );
+
+        $im = imagecreatetruecolor($x, $y + 200);
+        imagecopymerge($im, $coverBnf, 0, 0, 0, 0, $x, $y, 100);
+
+        $copyright = imagecreatetruecolor($x, 200);
+        $white = imagecolorallocate($im, 0xFF, 0xFF, 0xFF);
+        $black = imagecolorallocate($im, 0x00, 0x00, 0x00);
+        $font_file = './arial.ttf';
+
+        imagefilledrectangle($copyright, 0, 0, $x, 200, $white);
+        imagefttext($copyright, 13, 0, 105, 55, $black, $font_file, 'Source : BnF');
+        imagecopymerge($im, $copyright, 0, $y, 0, 0, $x, $y, 100);
+
+        imagejpeg($im, "sortie.jpg");
+        exit;
+    }
